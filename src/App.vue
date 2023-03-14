be56e02f6248d69a12fcd9517e681ed9681c2cad
evelyncaraujo <evyaraujoifc@gmail.com>
2023-03-14 11:48:23 -0300

    proj

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script> 
+// Será apresentado a seguir
+</script>
+
+<template> 
+<button @click="incrementarcontador">incrementar</button>
+<button @click="decrementarcontador">decrementar</button>
+<p> Valor do contador é: {{ contador }}</p>
+</template>
+<style scoped>
+button{
+    font-weight: bold;
+}
+</style>
